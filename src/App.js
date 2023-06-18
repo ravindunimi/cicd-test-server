a5f57d07f5411a36a86ac31058f470c7dfc25777
ravindunimi <ravindu@nimidev.com>
2023-06-18 18:24:57 +0530

    region added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Hello CI-CD Project!!!Pipeline
+       Hello CI-CD Project   Pipeline
       </header>
     </div>
   );

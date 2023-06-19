c07553e3e532bc8d9391b85dfb22cfe59096d7fb
ravindunimi <ravindu@nimidev.com>
2023-06-19 11:21:17 +0530

    modified Pipeline

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello CI-CD Project Pipeline 123!!!</header>
+      <header className="App-header">Hello CI-CD Project Pipeline!!!</header>
     </div>
   );
 }

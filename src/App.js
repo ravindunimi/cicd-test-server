076c1917c113143b8db1af129e90cfce4abc6d91
ravindunimi <118248445+ravindunimi@users.noreply.github.com>
2023-06-26 13:38:17 +0530

    Changed color as aqua

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function AppTest() {
 
   return (
     <div>
-      <header style={{ backgroundColor: "aqua", height: "60px" }}>
+      <header style={{ backgroundColor: "aqua", height: "70px" }}>
         <h1>New App Release...!</h1>
       </header>
 
